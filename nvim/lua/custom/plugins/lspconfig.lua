d7f5c7b96466fec7ee905b485f9383aed80017e9
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2022-09-11 22:54:32 -0500

    Changes

--- nvim/lua/custom/plugins/lspconfig.lua
+++ nvim/lua/custom/plugins/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "rust_analyzer", "clangd"}
+local servers = { "html", "rust_analyzer", "clangd", "texlab"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
